582515c2b43eb39682c87a305a52dfc359be6d5e
bisaraid <fazafa.id@gmail.com>
2025-10-21 13:27:54 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
-
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: 'class',
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  theme: { extend: {} },
-  plugins: [],
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          50: '#eef2ff',
+          500: '#4f46e5'
+        }
+      }
+    }
+  },
+  plugins: []
 }
