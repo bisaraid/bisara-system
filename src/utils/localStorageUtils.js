c683f5133d43b96b22f3a07f4b129313dfb25e02
bisaraid <fazafa.id@gmail.com>
2025-10-21 13:18:31 +0700

    Add localStorage utility functions for theme and data

--- src/utils/localStorageUtils.js
+++ src/utils/localStorageUtils.js
@@ -1,1 +1,11 @@
+export const getTheme = () =>
+  localStorage.getItem('theme') || 'light'
 
+export const setTheme = (mode) =>
+  localStorage.setItem('theme', mode)
+
+export const loadData = (key) =>
+  JSON.parse(localStorage.getItem(key)) || []
+
+export const saveData = (key, data) =>
+  localStorage.setItem(key, JSON.stringify(data))
