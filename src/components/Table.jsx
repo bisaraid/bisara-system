86375c02a8f41fbbbcc3368574ecfdb251fd0c46
bisaraid <fazafa.id@gmail.com>
2025-10-21 13:42:11 +0700

    Update Table.jsx

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,1 +1,12 @@
+import React from 'react'
+
+export default function Table({ children }) {
+  return (
+    <div className="overflow-x-auto bg-white dark:bg-gray-800 rounded shadow">
+      <table className="min-w-full">
+        {children}
+      </table>
+    </div>
+  )
+}
 
