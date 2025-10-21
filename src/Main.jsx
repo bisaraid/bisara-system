426baa19241666973ea1f3befa734920ec71fc9f
bisaraid <fazafa.id@gmail.com>
2025-10-21 23:41:26 +0700

    Update Main.jsx

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,16 +1,13 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter } from 'react-router-dom' // gunakan HashRouter biar aman di GitHub Pages
+import { HashRouter } from 'react-router-dom'
 import App from './App'
-import './Index.css'
+import './index.css'
 
-// Apply theme from localStorage on load
+// apply theme from localStorage
 const theme = localStorage.getItem('theme') || 'light'
-if (theme === 'dark') {
-  document.documentElement.classList.add('dark')
-} else {
-  document.documentElement.classList.remove('dark')
-}
+if (theme === 'dark') document.documentElement.classList.add('dark')
+else document.documentElement.classList.remove('dark')
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
