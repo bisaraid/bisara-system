a3ad3bdc7f459669dfa7acd0b31bf6661635d3b8
bisaraid <fazafa.id@gmail.com>
2025-10-21 14:52:41 +0700

    Rename index.css to Index.css in Main.jsx

--- src/Main.jsx
+++ src/Main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
-import './index.css'
+import './Index.css'
 
 // Apply theme from localStorage on load
 const theme = localStorage.getItem('theme') || 'light'
