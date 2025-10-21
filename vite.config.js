69f80bf71a91955b173f8e79b8db8cb5da804275
bisaraid <fazafa.id@gmail.com>
2025-10-21 15:08:19 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/bisara-system/' // sesuaikan saat deploy ke GitHub Pages
+  base: '/bisara-system/', // sesuaikan saat deploy ke GitHub Pages
 })
 
