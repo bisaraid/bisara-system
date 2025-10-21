f5df0b602fbe5e4c832bc738a47200f5d01316af
bisaraid <fazafa.id@gmail.com>
2025-10-21 13:35:19 +0700

    Add DashboardLayout component with Navbar and Sidebar

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -1,1 +1,16 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Sidebar from '../components/Sidebar'
+
+export default function DashboardLayout({ children }) {
+  return (
+    <div className="min-h-screen bg-gray-50 dark:bg-gray-900">
+      <Navbar />
+      <div className="flex">
+        <Sidebar />
+        <main className="flex-1 p-6">{children}</main>
+      </div>
+    </div>
+  )
+}
 
